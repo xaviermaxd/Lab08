b9b6b69af796972855a42d731830df286684f865
Alumno <Alumno@LAB1509-17>
2023-11-03 16:43:23 -0500

    Agregar archivos de proyecto.

---
+++ lab08/lab08/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab08
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new BatterDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
